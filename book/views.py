36fbba9bea49e1145dbe8dfe68e26c5f59e0e55e
kenkono <kono.k@dynamo-tech.com>
2019-03-20 16:06:40 +0700

    make urls

--- book/views.py
+++ book/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Index")
+
+
+def detail(request, book_id):
+    return HttpResponse("detail %s" % book_id)
+
+
+def edit(request, book_id):
+    return HttpResponse("edit %s" % book_id)
+
+
+def delete(request, book_id):
+    return HttpResponse("delete %s" % book_id)
+
+
+def create(request):
+    return HttpResponse("create")
+
+
+def mypage(request):
+    return HttpResponse("mypage")
+
+
+def login(request):
+    return HttpResponse("login")
+
+
+def logout(request):
+    return HttpResponse("logout")
+
+
+def signin(request):
+    return HttpResponse("siginin")
